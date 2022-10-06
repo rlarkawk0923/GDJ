4a2007c5b8d95d4136d9daa652fb7c35784309a2
rlarkawk0923 <giwon0923@naver.com>
2022-10-06 19:51:32 +0900

    2022-10-06

---
+++ jspstudy/01_Servlet/src/main/java/ex03/AnchorServlet.java
@@ -0,0 +1,55 @@
+package ex03;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/AnchorServlet")
+public class AnchorServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public AnchorServlet() {
+        super();
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// 요청
+		request.setCharacterEncoding("UTF-8");
+		
+		// 요청 파라미터
+		int a = 0, b = 0;
+		String strA = request.getParameter("a");
+		if(strA != null) {
+			a = Integer.parseInt(strA);
+		}
+		String strB = request.getParameter("b");
+		if(strB != null) {
+			b = Integer.parseInt(strB);
+		}
+				
+		// 응답
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>Hello World</h1>");
+		out.println("<h1>"+a+"+"+b+"="+(a+b)+"</h1>");
+		out.flush();
+		out.close();
+		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
---
+++ webstudy/UI/login.js
@@ -0,0 +1,22 @@
+// 비밀번호 입력 없이 Sign Im 버튼 누르면
+// 경고창 띄우기 (비밀번호를 입력하세요.)
+
+document.getElementById('btn_signin').onclick = function(event){
+    var pw = document.getElementById('pw');
+    if(pw.value == ''){
+        alert('비밀번호를 입력하세요.');
+        event.preventDefault(); // 기본동작 막기
+        return;
+    }
+}
+document.getElementById('id').onkeyup = function(event){
+    var id = document.getElementById('id');
+    var id_msg = document.getElementById('id_msg');
+    if(id.value.length ==0){
+        id_msg.textContent = '';
+    } else if(id.value.length < 4){
+        id_msg.textContent = '아이디는 4자 이상입니다,';
+    } else if(id.value.lenth >= 4){
+        id_msg.textContent = '정상적인 아이디입니다.';
+    }
+}
