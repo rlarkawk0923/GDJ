37f9c78f81ccc8ef1743ac17e1890c310699dea7
rlarkawk0923 <giwon0923@naver.com>
2022-10-19 19:50:07 +0900

    2022-10-19

---
+++ jspstudy/04_DBCP/src/main/java/repository/BoardDao.java
@@ -0,0 +1,175 @@
+package repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import domain.Board;
+
+public class BoardDao {
+	// field
+	private Connection con;
+	private PreparedStatement ps;
+	private ResultSet rs;
+	private String sql;
+
+	// connection pool 관리
+	private DataSource dataSource;
+
+	// singleton - pattern // 연습 많이하기
+	// 오직 하나의 객체만을 생성할 수 있는 클래스
+	// 한 번의 메모리만 할당하고 그 할당한 메모리에 대해 객체로 관리하기 위함
+	// 외부에서 호출 시 new를 사용하지 않고 getInstance를 사용하여 객체를 생성하거나 반환받습니다.
+	// 최초 호출이라면 new CompanyInfo()가 return 되어 사용할 것이고 두 번째 이후부터는 기존에 생성된 instance를
+	// 반환받을 것입니다.
+	private static BoardDao dao = new BoardDao();
+
+	private BoardDao() {
+		try {
+			// DataSource 객체 생성
+			// context.xml에서 name="jdbc/oracle11g"인 Resource를 찾아서 생성(JNDI)
+			Context ctx = new InitialContext();
+			Context envCtx = (Context) ctx.lookup("java:comp/env");
+			dataSource = (DataSource) envCtx.lookup("jdbc/oracle11g");
+			// dataSource = (DataSource)ctx.lookup("java:comp/env/jdbc/oracle11g");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static BoardDao getInstance() {
+		return dao;
+	}
+
+	// method
+	// 1. 접속 / 자원 해제
+	public void close(Connection con, PreparedStatement ps, ResultSet rs) {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (ps != null) {
+				ps.close();
+			}
+			if (con != null) {
+				con.close();
+			} // Connection Pool의 close()는 Connection 종료가 아닌 Connection 반환으로 동작
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// 2. 목록 반환하기
+	public List<Board> selectAllBoards() {
+		List<Board> boards = new ArrayList<Board>();
+		try {
+			con = dataSource.getConnection(); // CP로부터 Connection 대여
+			sql = "SELECT BOARD_NO, TITLE, CONTENT, CREATE_DATE FROM BOARD ORDER BY BOARD_NO DESC";
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery(); // SELECT문은 executeQuery() 사용
+			while (rs.next()) {
+				Board board = new Board();
+				board.setBoard_no(rs.getInt(1));// rs.getInt("Board_No")
+				board.setTitle(rs.getString(2));// rs.getString("TITLE")
+				board.setContent(rs.getString(3));// rs.getString("CONTENT")
+				board.setCreate_date(rs.getDate(4));// rs.getDate("CREATE_DATE")
+
+				// 가져온 게시글을 리스트에 추가함
+				boards.add(board);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(con, ps, rs);
+		}
+		return boards;
+	}
+	
+	// 3. 상세보기
+	public Board selectBoardByNo(int board_no) {
+		Board board = null;
+		try {
+			con = dataSource.getConnection();
+			sql = "SELECT BOARD_NO, TITLE, CONTENT, CREATE_DATE FROM BOARD WHERE BOARD_NO = ?";
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, board_no); // 1번째 물음표(?)에 board_no 전달하기
+			rs = ps.executeQuery();// SELECT문은 executeQuery()사용
+			if(rs.next()){ //상세보기, rs에 next()한번 필요
+				board = new Board();
+				board.setBoard_no(rs.getInt(1));// rs.getInt("Board_No")
+				board.setTitle(rs.getString(2));// rs.getString("TITLE")
+				board.setContent(rs.getString(3));// rs.getString("CONTENT")
+				board.setCreate_date(rs.getDate(4));// rs.getDate("CREATE_DATE")
+				
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			close(con, ps, rs);
+		}
+		return board;
+	}
+	
+	// 4. 게시글 삽입
+	public int insertBoard(Board board) {
+		int result = 0;
+		try {
+			con = dataSource.getConnection();
+			sql = "INSERT INTO BOARD VALUES(BOARD_SEQ.NEXTVAL, ?, ?, SYSDATE)";
+			ps = con.prepareStatement(sql);
+			ps.setString(1, board.getTitle());
+			ps.setString(2, board.getContent());
+			result = ps.executeUpdate(); // INSERT문은 executeUpdate() 메소드 사용
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			close(con, ps, null);
+		}
+		return result;
+	}
+	
+	// 5. 게시글 수정
+	public int updateBoard(Board board) {
+		int result = 0;
+		try {
+			con = dataSource.getConnection();
+			sql = "UPDATE BOARD SET TITLE = ?, CONTENT = ? WHERE BOARD_NO = ?";
+			ps = con.prepareStatement(sql);
+			ps.setString(1, board.getTitle());
+			ps.setString(2, board.getContent());
+			ps.setInt(3, board.getBoard_no());
+			result = ps.executeUpdate(); // UPDATE문은 executeUpdate()메소드 사용
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			close(con, ps, null);
+		}
+		return result;
+
+	}
+	
+	// 6. 게시글 삭제
+	public int deleteBoard(int board_no) {//int_board_no pk값
+		int result = 0;
+		try {
+			con = dataSource.getConnection();
+			sql = "DELETE FROM BOARD WHERE BOARD_NO = ?";
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, board_no);// 첫번째물음표에 보드넘버 넣어라
+			result=ps.executeUpdate();//executeDelete아님executeInsert아님
+				
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			close(con, ps, null);
+		}
+		return result;
+	}
+}
---
+++ jspstudy/04_DBCP/src/main/java/service/BoardRemoveService.java
@@ -0,0 +1,34 @@
+package service;
+
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.ActionForward;
+import repository.BoardDao;
+
+public class BoardRemoveService implements BoardService {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		// 요청 파라미터
+		Optional<String> opt = Optional.ofNullable(request.getParameter("board_no"));
+		//int board_no = Integer.parseInt(request.getParameter("board_no")); integerparseint는 null값 허용안되기 때문에 option으로 감싼것
+		int board_no = Integer.parseInt(opt.orElse("0")); // 0을 dao 측에 전달하면 실행결과가 0=>삭제되지 않는 번호, 없는테이블
+		
+		//DB로 board_no 보내서 삭제
+		int result = BoardDao.getInstance().deleteBoard(board_no);
+		
+		// 삭제 성공/실패 여부 콘솔에 출력
+		System.out.println("삭제 여부 : " + result);
+		
+		// 어디로 어떻게
+		ActionForward af = new ActionForward();
+		af.setView(request.getContextPath() + "/board/list.do"); // Redirect할때는 매핑으로 이동 목록서비스 돌려서 새 목록 가져와야함 특정 jsp로 이동하지 않음
+		af.setRedirect(true); // DELETE 이후에는 Redirect(두번요청)하는게 기본
+		return af;
+	}
+
+}
