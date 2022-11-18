f3153a0edd438456e634d8070f209acd2bb9f138
rlarkawk0923 <giwon0923@naver.com>
2022-11-18 17:51:41 +0900

    2022-11-18

--- springstudy/13_Pjt_User/src/main/java/com/gdu/app13/service/UserServiceImpl.java
+++ springstudy/13_Pjt_User/src/main/java/com/gdu/app13/service/UserServiceImpl.java
@@ -27,6 +27,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
